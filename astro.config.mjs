e0874db9ae6711d5763249043c3c4ba624ef080b
Catalin Urcan <catalinurcan9@gmail.com>
2024-10-15 03:37:01 +0300

    Navbar, Link, Collapse, Button

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from "astro/config";
-import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 
-export default defineConfig({ integrations: [svelte(), tailwind()] });
+export default defineConfig({ integrations: [tailwind()] });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,10 @@ import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/preline/preline.js",
+  ],
   theme: {
     extend: {
       fontFamily: {
@@ -20,14 +23,22 @@ export default {
     },
     colors: {
       primary: "#6B0808",
+      primaryHover: "#861616",
       secondary: "#3E5F3D",
+      secondaryHover: "#487247",
       accent: "#360914",
+      accentHover: "#7D1E37",
       text: "#180000",
       background: "#FAFAFA",
     },
     container: {
       center: true,
+      padding: {
+        DEFAULT: "1rem",
+        md: "2.75rem",
+        xl: "4.5rem",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms"), require("preline/plugin")],
 };
